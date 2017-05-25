831d542e47af034b51ad667154aef7e7e26af076
Rick Love <ricklove@outlook.com>
2017-05-25 22:09:59 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,14 +1,16 @@
+// var arr = [];
+
 function forLoop(array){
 
-  for (let i = 0; i < 25; i++) {
-    if (i === 1) {
-      array.push(`I am ${i} strange loop.`)
-    } else {
-      array.push(`I am ${i} strange loops.`)
+    for(i=0; i < 25; i++){
+      if(i === 1){
+        array.push(`I am ${i} strange loop.`);
+      }
+      else array.push(`I am ${i} strange loops.`);
     }
+    return(array);
   }
-  return array;
-}
+
 
 function whileLoop(n){
   while (n > 0){
